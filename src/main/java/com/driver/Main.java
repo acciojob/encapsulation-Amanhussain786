cddf26b87edb419e7c5e9b4c9a7c9d81bf0b7a86
Amanhussain786 <hussain786aman123@gmail.com>
2023-02-15 14:28:40 +0530

    Newone

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args)
+    {
+        RWOnly obj = new RWOnly();
+        obj.setName("vijay");
+     //getting value of the name member
+        System.out.println(obj.getName());
+    }
 }
